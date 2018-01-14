b71807c2eb045460a7ac9a39c5845ae7df5587d4
Volodin Sergey <s.volodin@corp.mail.ru>
2018-01-14 20:41:55 +0300

    fiexd installation

---
+++ digit.ts
@@ -0,0 +1,8 @@
+/**
+ *  Лабораторная работа по моделированию №1
+ *  Построение марковской цепи при дискретном времени
+ */
+
+import * as readLine from 'readline-sync';
+
+const n: number = readLine.questionInt('Введите число состояний: \n');
